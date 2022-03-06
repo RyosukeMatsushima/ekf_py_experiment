6695f157d53c91a2c71c178c73fa6d40ebc7c982
RyosukeMatsushima <m.ryosuke@me.com>
2022-03-06 22:19:05 +0900

    add simulate.py for test ekf_2d
    simulate.py need to verify the correction

--- ekf_2d/ekf.py
+++ ekf_2d/ekf.py
@@ -5,7 +5,7 @@ class EKF:
     def __init__(self):
 
         # states: [position_x, position_y, velocity_x, velocity_y, yaw]
-        self.x = np.array([0.0, 0.0, 0.0, 0.0, 0.0]).T
+        self.x = np.array([[0.0, 0.0, 0.0, 0.0, 0.0]]).T
         self.P = np.array([[1000, 0.0, 0.0, 0.0, 0.0],
                            [0.0, 1000, 0.0, 0.0, 0.0],
                            [0.0, 0.0, 1000, 0.0, 0.0],
@@ -27,7 +27,7 @@ class EKF:
 
         A, B_u, B = self.get_liner_model(self.x, u, dt)
 
-        x_predict = A @ self.x + B @ u
+        x_predict = A @ self.x + B_u @ u
         P_predict = A @ self.P @ A.T + self.rho_v * B @ B.T
 
         return x_predict, P_predict
@@ -39,30 +39,27 @@ class EKF:
         self.last_update_t = t
         x_p, P_p = self.predict(u, dt)
 
-        P_p_inv = np.linalg.inv(P_p)
-        denominator = self.C @ P_p_inv @ self.C.T + self.R
+        denominator = self.C @ P_p @ self.C.T + self.R
 
-        G = P_p_inv @ self.C.T @ np.linalg.inv(denominator)
-
-        self.x = x_p + G @ (y - C @ x_p)
-
+        G = P_p @ self.C.T @ np.linalg.inv(denominator)
+        self.x = x_p + G @ (y - self.C @ x_p)
         return self.x
 
     # return liner model matrix A, B_u, B
     def get_liner_model(self, x, u, dt):
 
-        a_x = u[0]
-        a_y = u[1]
-        yaw = x[4]
+        a_x = u[0][0]
+        a_y = u[1][0]
+        yaw = x[4][0]
 
         A = np.array([[0.0, 0.0, 1.0, 0.0, 0.0],
                       [0.0, 0.0, 0.0, 1.0, 0.0],
                       [0.0, 0.0, 0.0, 0.0, -a_x * np.sin(yaw) - a_y * np.cos(yaw)],
-                      [0.0, 0.0, 0.0, 0.0, a_x * np.cos(yaw) - a_y * np.sin(yaw)]])
-                      [0.0, 0.0, 0.0, 0.0, 0.0],
+                      [0.0, 0.0, 0.0, 0.0, a_x * np.cos(yaw) - a_y * np.sin(yaw)],
+                      [0.0, 0.0, 0.0, 0.0, 0.0]])
 
         B_u = np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 1.0]])
 
-        B = np.array([[0.1], [0.1], [0.1], [0.1], [0.1]])
+        B = np.array([[0.1], [0.1], [0.01], [0.01], [0.02]])
 
         return A, B_u, B
---
+++ ekf_2d/simulate.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+import pathlib
+
+current_dir = pathlib.Path(__file__).resolve().parent
+sys.path.append( str(current_dir) + '/../' )
+from physics_simulator.rigid_body_2d.rigid_body_2d import RigidBody2D
+from ekf import EKF
+
+ekf = EKF()
+
+X = 0.0
+X_dot = 1.0
+Y = 0.0
+Y_dot = 1.0
+yaw = 0.0
+yaw_dot = 1.0
+init_state = (X, X_dot, Y, Y_dot, yaw, yaw_dot)
+rigidBody2D = RigidBody2D(init_state)
+
+time = 0.
+dt = 10**(-2)
+max_step = 60 * 10**(2) + 1
+
+df = pd.DataFrame(columns=['time',
+                           'X',
+                           'X_dot',
+                           'Y',
+                           'Y_dot',
+                           'yaw',
+                           'yaw_dot'])
+
+ekf_df = pd.DataFrame(columns=['time',
+                               'X',
+                               'X_dot',
+                               'Y',
+                               'Y_dot',
+                               'yaw'])
+
+# def add_data(df):
+for s in range(0, max_step):
+    time = s * dt
+    tmp_data = tuple([time]) + rigidBody2D.state
+    print(time)
+    print('tmp_data')
+    print(tmp_data)
+    tmp_se = pd.Series(tmp_data, index=df.columns)
+    df = df.append(tmp_se, ignore_index=True)
+    rigidBody2D.step(dt)
+
+    # generate sensor_data
+    imu_data = rigidBody2D.get_sensor_data()
+    imu_data = np.array([ [imu_data['accel'][0]],
+                          [imu_data['accel'][1]],
+                          [imu_data['angle_rate']] ])
+
+    pos_data = np.array([ [rigidBody2D.state[0]],
+                          [rigidBody2D.state[1]] ])
+
+    # update ekf
+    estimated_state = ekf.update( pos_data, imu_data, time )
+    tmp_data = tuple([time]) + tuple( val[0] for val in estimated_state )
+    print('tmp_data')
+    print(tmp_data)
+    tmp_se = pd.Series(tmp_data, index=ekf_df.columns)
+    ekf_df = ekf_df.append(tmp_se, ignore_index=True)
+
+df.plot(x='X', y='Y')
+df.plot(x='time', y='X')
+df.plot(x='time', y='Y')
+df.plot(x='time', y='yaw')
+plt.show()
+
+ekf_df.plot(x='X', y='Y')
+ekf_df.plot(x='time', y='X')
+ekf_df.plot(x='time', y='Y')
+ekf_df.plot(x='time', y='yaw')
+plt.show()
+
