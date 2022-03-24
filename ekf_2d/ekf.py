a7744e2908d68447a9a7d2ae832f4682fb42916b
RyosukeMatsushima <m.ryosuke@me.com>
2022-03-24 23:04:00 +0900

    ekf.py - fix ekf logical bugs

--- ekf_2d/ekf.py
+++ ekf_2d/ekf.py
@@ -6,15 +6,17 @@ class EKF:
 
         # states: [position_x, position_y, velocity_x, velocity_y, yaw]
         self.x = np.array([[0.0, 0.0, 0.0, 0.0, 0.0]]).T
-        self.P = np.array([[1000, 0.0, 0.0, 0.0, 0.0],
-                           [0.0, 1000, 0.0, 0.0, 0.0],
-                           [0.0, 0.0, 1000, 0.0, 0.0],
-                           [0.0, 0.0, 0.0, 1000, 0.0],
-                           [0.0, 0.0, 0.0, 0.0, 1000]])
-        self.rho_v = 1.0
+        self.P = np.array([[1, 0.0, 0.0, 0.0, 0.0],
+                           [0.0, 1, 0.0, 0.0, 0.0],
+                           [0.0, 0.0, 1, 0.0, 0.0],
+                           [0.0, 0.0, 0.0, 1, 0.0],
+                           [0.0, 0.0, 0.0, 0.0, 0.0000001]])
+        self.Q = np.array([[0.1, 0.0, 0.0],
+                           [0.0, 0.1, 0.0],
+                           [0.0, 0.0, 0.01]])
         self.C = np.array([[1.0, 0.0, 0.0, 0.0, 0.0],
                            [0.0, 1.0, 0.0, 0.0, 0.0]])
-        self.R = np.array([[1.0], [1.0]])
+        self.R = np.array([[0.01], [0.01]])
 
         self.last_update_t = 0.0
 
@@ -24,10 +26,10 @@ class EKF:
     # return predict states
     def predict(self, u, dt):
 
-        A, B_u, B = self.get_liner_model(self.x, u, dt)
+        A, B, A_d = self.get_liner_model(self.x, u, dt)
 
-        x_predict = A @ self.x + B_u @ u
-        P_predict = A @ self.P @ A.T + self.rho_v * B @ B.T
+        x_predict = A_d @ self.x + B @ u
+        P_predict = A @ self.P @ A.T + B @ self.Q @ B.T
 
         return x_predict, P_predict
 
@@ -42,6 +44,12 @@ class EKF:
 
         G = P_p @ self.C.T @ np.linalg.inv(denominator)
         self.x = x_p + G @ (y - self.C @ x_p)
+
+        self.P = (np.eye(len(self.x)) - G @ self.C) @ P_p
+
+        print(self.P)
+        self.x[4] = self.yaw_correction(self.x[4])
+
         return self.x
 
     # return liner model matrix A, B_u, B
@@ -49,16 +57,38 @@ class EKF:
 
         a_x = u[0][0]
         a_y = u[1][0]
+        yaw_rate = u[2][0]
         yaw = x[4][0]
 
         A = np.array([[0.0, 0.0, 1.0, 0.0, 0.0],
                       [0.0, 0.0, 0.0, 1.0, 0.0],
-                      [0.0, 0.0, 0.0, 0.0, -a_x * np.sin(yaw) - a_y * np.cos(yaw)],
-                      [0.0, 0.0, 0.0, 0.0, a_x * np.cos(yaw) - a_y * np.sin(yaw)],
+                      [0.0, 0.0, 0.0, 0.0, yaw_rate * (-a_x * np.sin(yaw) - a_y * np.cos(yaw))],
+                      [0.0, 0.0, 0.0, 0.0, yaw_rate * (a_x * np.cos(yaw) - a_y * np.sin(yaw))],
                       [0.0, 0.0, 0.0, 0.0, 0.0]])
+        A = np.eye(len(self.x)) + A * dt
 
-        B_u = np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 1.0]])
+        A_d = np.array([[0.0, 0.0, 1.0, 0.0, 0.0],
+                      [0.0, 0.0, 0.0, 1.0, 0.0],
+                      [0.0, 0.0, 0.0, 0.0, 0.0],
+                      [0.0, 0.0, 0.0, 0.0, 0.0],
+                      [0.0, 0.0, 0.0, 0.0, 0.0]])
+        A_d = np.eye(len(self.x)) + A_d * dt
 
-        B = np.array([[0.1], [0.1], [0.01], [0.01], [0.02]])
 
-        return A, B_u, B
+        B = np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [np.cos(yaw), -np.sin(yaw), 0.0], [np.sin(yaw), np.cos(yaw), 0.0], [0.0, 0.0, 1.0]]) * dt
+
+        return A, B, A_d
+
+    def yaw_correction(self, yaw):
+        return yaw - int(yaw/np.pi) * np.pi
+
+class OnlyIntegral(EKF):
+
+    def update(self, y, u, t):
+        dt = t - self.last_update_t
+        self.last_update_t = t
+        x_p, P_p = self.predict(u, dt)
+        self.x = x_p
+        self.x[4] = self.yaw_correction(self.x[4])
+
+        return self.x
--- ekf_2d/simulate.py
+++ ekf_2d/simulate.py
@@ -8,77 +8,114 @@ current_dir = pathlib.Path(__file__).resolve().parent
 sys.path.append( str(current_dir) + '/../' )
 from physics_simulator.rigid_body_2d.rigid_body_2d import RigidBody2D
 from ekf import EKF
+from ekf import OnlyIntegral
 
 ekf = EKF()
+onlyIntegral = OnlyIntegral()
 
 X = 0.0
-X_dot = 1.0
+X_dot = 0.0
 Y = 0.0
-Y_dot = 4.0
+Y_dot = 0.0
 yaw = 0.0
-yaw_dot = 1.0
+yaw_dot = 0.0
 init_state = (X, X_dot, Y, Y_dot, yaw, yaw_dot)
 rigidBody2D = RigidBody2D(init_state)
-rigidBody2D.input = (1.0, 0.1, 0.00002)
+#rigidBody2D.input = (0.001, 0.001, 0.00002)
+rigidBody2D.input = (1.0, 1.0, 0.0)
+rigidBody2D.input = (0.001, 0.001, 0.0)
 
 time = 0.
 dt = 10**(-2)
-max_step = 10 * 10**(2) + 1
+max_step = 30 * 10**(2) + 1
 
-df = pd.DataFrame(columns=['time',
-                           'X',
-                           'X_dot',
-                           'Y',
-                           'Y_dot',
-                           'yaw',
-                           'yaw_dot'])
-
-ekf_df = pd.DataFrame(columns=['time',
-                               'X',
-                               'Y',
-                               'X_dot',
-                               'Y_dot',
-                               'yaw'])
+state_df = pd.DataFrame(columns=['time',
+                                 'X',
+                                 'X_dot',
+                                 'Y',
+                                 'Y_dot',
+                                 'yaw',
+                                 'yaw_dot',
+                                 'est_X',
+                                 'est_Y',
+                                 'est_X_dot',
+                                 'est_Y_dot',
+                                 'est_yaw',
+                                 'X_err',
+                                 'Y_err',
+                                 'yaw_err',
+                                 'only_integ_est_X',
+                                 'only_integ_est_Y',
+                                 'only_integ_est_X_dot',
+                                 'only_integ_est_Y_dot',
+                                 'only_integ_est_yaw',
+                                 'only_integ_X_err',
+                                 'only_integ_Y_err',
+                                 'only_integ_yaw_err'])
 
 # def add_data(df):
 for s in range(0, max_step):
     time = s * dt
-    tmp_data = tuple([time]) + rigidBody2D.state
     print(time)
-    print('tmp_data')
-    print(tmp_data)
-    tmp_se = pd.Series(tmp_data, index=df.columns)
-    df = df.append(tmp_se, ignore_index=True)
 
     # generate sensor_data
     imu_data = rigidBody2D.get_sensor_data()
-    imu_data = np.array([ [imu_data['accel'][0]],
-                          [imu_data['accel'][1]],
-                          [imu_data['angle_rate']] ])
+    imu_data = np.array([ [imu_data['accel'][0] + 0.1 * np.random.randn()],
+                          [imu_data['accel'][1] + 0.1 * np.random.randn()],
+                          [imu_data['angle_rate'] + 0.000001 * np.random.randn()] ])
 
-    pos_data = np.array([ [rigidBody2D.state[0]],
-                          [rigidBody2D.state[2]] ])
+    pos_data = np.array([ [rigidBody2D.state[0] + 0.01 * np.random.randn()],
+                          [rigidBody2D.state[2] + 0.01 * np.random.randn()] ])
 
     # update ekf
     estimated_state = ekf.update( pos_data, imu_data, time )
-    tmp_data = tuple([time]) + tuple( val[0] for val in estimated_state )
-    print('tmp_data')
-    print(tmp_data)
-    tmp_se = pd.Series(tmp_data, index=ekf_df.columns)
-    ekf_df = ekf_df.append(tmp_se, ignore_index=True)
 
+    # about only integral estimation
+    only_integ_state = onlyIntegral.update( pos_data, imu_data, time )
+
+    tmp_data = tuple([time]) \
+             + rigidBody2D.state \
+             + tuple( val[0] for val in estimated_state ) \
+             + (rigidBody2D.state[0] - estimated_state[0], \
+                rigidBody2D.state[2] - estimated_state[1], \
+                rigidBody2D.state[4] - estimated_state[4]) \
+             + tuple( val[0] for val in only_integ_state ) \
+             + (rigidBody2D.state[0] - only_integ_state[0], \
+                rigidBody2D.state[2] - only_integ_state[1], \
+                rigidBody2D.state[4] - only_integ_state[4])
+    tmp_se = pd.Series(tmp_data, index=state_df.columns)
+    state_df = state_df.append(tmp_se, ignore_index=True)
+ 
     rigidBody2D.step(dt)
 
+def show_plt(title, df, labels):
+    fig = plt.figure(figsize=(6, 4))
+    ax = fig.add_subplot(111)
+    ax.set_title(title)
 
-ekf_df.plot(x='X', y='Y')
-df.plot(x='X', y='Y')
+    for label in labels:
+        ax.plot(df[label[0]], df[label[1]], label=label[2])
 
-ekf_df.plot(x='time', y='X')
-df.plot(x='time', y='X')
-ekf_df.plot(x='time', y='Y')
-df.plot(x='time', y='Y')
+    plt.legend()
 
-ekf_df.plot(x='time', y='yaw')
-df.plot(x='time', y='yaw')
+show_plt('X Y', state_df, [['X', 'Y', 'true'],
+                           ['est_X', 'est_Y', 'est'],
+                           ['only_integ_est_X', 'only_integ_est_Y', 'only_integ_est']])
+show_plt('X', state_df, [['time', 'X', 'true'],
+                         ['time', 'est_X', 'est'],
+                         ['time', 'only_integ_est_X', 'only_integ_est']])
+show_plt('Y', state_df, [['time', 'Y', 'true'],
+                         ['time', 'est_Y', 'est'],
+                         ['time', 'only_integ_est_Y', 'only_integ_est']])
+show_plt('yaw', state_df, [['time', 'yaw', 'true'],
+                           ['time', 'est_yaw', 'est'],
+                           ['time', 'only_integ_est_yaw', 'only_integ_est']])
+
+show_plt('X_err', state_df, [['time', 'X_err', 'X_err'],
+                             ['time', 'only_integ_X_err', 'only_integ_X_err']])
+show_plt('Y_err', state_df, [['time', 'Y_err', 'Y_err'],
+                             ['time', 'only_integ_Y_err', 'only_integ_Y_err']])
+show_plt('yaw_err', state_df, [['time', 'yaw_err', 'yaw_err'],
+                               ['time', 'only_integ_yaw_err', 'only_integ_yaw_err']])
+
 plt.show()
-
